0819a07c7136cb87d247ccca64aab1d9072c491c
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2025-05-11 11:49:26 -0500

    refactor: Se agrego descripción de una función

--- config/db.py
+++ config/db.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
 def get_db():
+    """Obtiene la conexión a la base de datos MongoDB"""
     client = MongoClient("mongodb://root:example@localhost:27017/")
     db = client["spendly"]
     return db
